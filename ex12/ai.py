fa6c72bd97a7fb32476d7ed673b6ab1902530c6b
noamohyon <46459412+noamohyon@users.noreply.github.com>
2019-01-17 14:08:27 +0200

    Update ai.py

--- ex12/ai.py
+++ ex12/ai.py
@@ -1,4 +1,5 @@
 from random import randint
+import game as game
 
 class AI:
     """
@@ -15,9 +16,9 @@ class AI:
         a method which finds a legal move which the AI can do.
         """
         if self.game.get_winner() is None:
-            guess = randint(0,len(self.game.board[0]))
+            guess = randint(0, game.Game.BOARD_COLUMNS - 1)
             while self.game.get_player_at(0, guess) is not None:
-                guess = randint(0,len(self.game.board[0]))
+                guess = randint(0,game.Game.BOARD_COLUMNS - 1)
             return guess
         raise Exception("No possible AI moves")
 
